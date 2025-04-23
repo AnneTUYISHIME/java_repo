e7ac8e1cc0ca332b03595d4169c5461c773304df
Tuyishime Anne <atuyishime28@gmail.com>
2025-04-23 11:07:14 +0200

    shape task using interface with polymorphism feature

--- Shapes/src/main/java/org/example/Main.java
+++ Shapes/src/main/java/org/example/Main.java
@@ -4,7 +4,18 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Circle c1= new Circle(5);
+        Shape shape = new Circle(5);
+        shape.getArea();
+        shape.Draw();
+
+        Shape shape2 = new Rectangle(5,5);
+        shape2.getArea();
+        shape2.Draw();
+        Shape shape3 = new Triangle(5,5);
+        shape3.getArea();
+        shape3.Draw();
+
+        /*Circle c1= new Circle(5);
         c1.getArea();
         c1.Draw();
         Triangle t1= new Triangle(5,5);
@@ -12,6 +23,6 @@ public class Main {
         t1.Draw();
         Rectangle r1= new Rectangle(5,5);
         r1.getArea();
-        r1.Draw();
+        r1.Draw();*/
     }
 }
